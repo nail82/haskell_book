fd520960083018e2011551362b5cd47a7006f310
tsatcher <ted.satcher@dr-inc.com>
2020-04-15 11:18:55 -0500

    Log parser examples

--- ch24/ChptEx/src/Ex5.hs
+++ ch24/ChptEx/src/Ex5.hs
@@ -1,2 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+-- Log file parser
 module Ex5 where
+
+import Text.Trifecta
+import Text.RawString.QQ
+
+commentExample :: String
+commentExample = "-- wheee a comment"
+
+logExample :: String
+logExample = "08:00 Breakfast"
+
+dateExample :: String
+dateExample = "# 2025-02-07 -- dates not nececessarily sequential"
+
+twoLine :: String
+twoLine = [r|
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+|]
