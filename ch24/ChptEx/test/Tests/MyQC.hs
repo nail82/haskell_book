73da530a01d992a2db85febbbb1c8c1032907f88
Ted Satcher <ted.satcher@gmail.com>
2020-05-01 15:50:02 -0500

    First generator for log parsing quickcheck.

--- ch24/ChptEx/test/Tests/MyQC.hs
+++ ch24/ChptEx/test/Tests/MyQC.hs
@@ -1,4 +1,14 @@
 module Tests.MyQC (runQC) where
 
+import Test.QuickCheck
+
+genMin :: Char -> Char -> Gen String
+genMin m mm = return $ m : mm : []
+
+minutes = [genMin m mm | m <- "012345", mm <- "0123456789"]
+
+minuteGen :: Gen String
+minuteGen = oneof minutes
+
 runQC :: IO ()
 runQC = putStrLn "=>QuickCheck tests not implemented.  Stay tuned.<="
