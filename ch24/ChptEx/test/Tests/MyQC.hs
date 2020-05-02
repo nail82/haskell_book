268eb44e1108b7a1d9937b7e3ba4e901402da162
Ted Satcher <ted.satcher@gmail.com>
2020-05-02 09:46:34 -0500

    Working on generators for time.

--- ch24/ChptEx/test/Tests/MyQC.hs
+++ ch24/ChptEx/test/Tests/MyQC.hs
@@ -2,21 +2,33 @@ module Tests.MyQC (runQC) where
 
 import Test.QuickCheck
 
+import Ex5
+
+type HHMM = String
+
 genMin :: Char -> Char -> String
 genMin m mm = m : mm : []
 
+-- A list of valid minutes
 minutes = [genMin m mm | m <- "012345", mm <- "0123456789"]
 
-hours = fmap showHour validHours
+-- A list of valid hours
+hours = fmap showHour [0..23]
 
-validHours :: [Int]
-validhours = [0..23]
+-- A list of valid hh:mm Generators
+allTimes = [timeF h m  | h <- hours, m <- minutes]
+
+timeF :: String -> String -> Gen HHMM
+timeF h m = return $ h <> ":" <> m
 
 showHour :: Int -> String
 showHour h
          | h < 10 = "0" <> show h
          | otherwise = show h
 
+hhmmGen :: Gen HHMM
+hhmmGen = oneof allTimes
 
 runQC :: IO ()
-runQC = putStrLn "=>QuickCheck tests not implemented.  Stay tuned.<="
+runQC = do
+  putStrLn "=>QuickCheck tests not implemented.  Stay tuned.<="
