a2d72d2193d37c1d224deb8ad33394813cbf2b35
Ted Satcher <ted.satcher@gmail.com>
2020-05-02 09:45:56 -0500

    Give a new resolver to ch14. Add a note or two.

--- ch14/chpt-qc/src/Lib.hs
+++ ch14/chpt-qc/src/Lib.hs
@@ -6,12 +6,14 @@ someFunc = putStrLn "someFunc"
 half :: Fractional a => a -> a
 half x = x / 2
 
+-- Linear search to determine if a list is sorted. From pg 575
+-- Uses a tuple to fold the list.
 listOrdered :: (Ord a) => [a] -> Bool
 listOrdered xs =
     snd $ foldr go (Nothing, True) xs
         where go _ status@(_, False) = status
               go y (Nothing, t)      = (Just y, t)
-              go y (Just x,  t)      = (Just y, x >= y)
+              go y (Just x,  _)      = (Just y, x >= y)
 
 
 funcAssociative :: (Eq a, Integral a) => (a -> a -> a) -> a -> a -> a -> Bool
