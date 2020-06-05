dc73b7a033b59a734e2a79e446ee344746fa2a5e
Ted Satcher <ted.satcher@dr-inc.com>
2020-06-05 10:01:59 -0500

    IPv4 parser implemented.

--- ch24/ChptEx/src/Ex6.hs
+++ ch24/ChptEx/src/Ex6.hs
@@ -9,3 +9,22 @@ import Text.Trifecta
 data IPAddress =
     IPAddress Word32
     deriving (Eq, Ord, Show)
+
+parseQuad :: Parser IPAddress
+parseQuad = do
+  cs <- sepBy1 parseAddressComponent dot
+  if length cs == 4 then return $ IPAddress $ shiftQuad cs 0
+  else fail "Too many components"
+
+parseAddressComponent :: Parser Word32
+parseAddressComponent = do
+  c <- integer
+  if 0 <= c && c < 256 then return $ fromInteger c
+  else fail "Component out of range"
+
+shiftQuad :: [Word32] -> Word32 -> Word32
+shiftQuad [] v = v
+shiftQuad (c:cs) v =
+    let s = length cs * 8
+        v' = (shift c s) .|. v
+    in shiftQuad cs v'
